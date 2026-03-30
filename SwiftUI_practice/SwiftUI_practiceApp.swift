ce10dbc94b3d155ada43227a8f0d412821cef447
kkt6394 <kkt6394@gmail.com>
2026-03-30 10:11:05 +0900

    Initial Commit

---
+++ SwiftUI_practice/SwiftUI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_practiceApp.swift
+//  SwiftUI_practice
+//
+//  Created by 김기태 on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
